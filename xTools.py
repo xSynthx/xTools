663b6ada70a41caf5ddf621f5f649c4ccdbf88eb
Synthx <synthx@protonmail.com>
2017-03-07 15:25:37 -0600

    Added a lot of things!

--- xTools.py
+++ xTools.py
@@ -19,38 +19,44 @@ color = {
     'DEFAULT'         : '\033[0m',
     'TWITTER_BLUE'            : '\033[38;5;33m',
 }
-version = 'V1.7'
+pythonVersions = '\n[1]python2\n[2]python3.5.2'
 toolList = '\n[1]Folder Locker\n[2]Easy SSH'
-listTools = 'Folder Locker, Easy SSH ' + color['RED'] + '[MORE COMING SOON!]' + color['DEFAULT']
-exitSentence = color['RED'] + '[Now Exiting!] ' + color['DEFAULT'] + 'xTools ' + version + '\n'
-credits = '©2017 Synthx\n' + color['TWITTER_BLUE'] + 'Twitter: @_Synthx' + color['DEFAULT']
+listTools = '''
++----------------+
+| Folder Locker  |
++----------------+
+| Easy SSH       |
++----------------+
+'''  + color['RED'] + '[MORE COMING SOON!]' + color['DEFAULT']
+exitSentence = color['RED'] + '[Now Exiting!] ' + color['DEFAULT'] + 'xTools\n'
+credits = '©2017 Synthx\n' + color['TWITTER_BLUE'] + 'Twitter: @_Synthx' + color['DEFAULT'] + '\n@danbatiste\n@NyteLife26'
 printHelp = color['MAGENTA'] + '''
-+------------+-------------------------------------+
-| clear      |  Clears the screen!                 |
-+------------+-------------------------------------+
-| exit       |  Exits xTools :/                    |
-+------------+-------------------------------------+
-| help       |  Shows this dialog                  |
-+------------+-------------------------------------+
-| open tool  |  Lets you choose what tool to open  |
-+------------+-------------------------------------+
-| tools      |  Shows the list of tools            |
-+------------+-------------------------------------+
-| info       |  Shows credits for xTools           |
-+------------+-------------------------------------+
++------------+---------------------------------------+
+| clear      |  Clears the screen!                   |
++------------+---------------------------------------+
+| exit       |  Exits xTools :/                      |
++------------+---------------------------------------+
+| help       |  Shows this dialog                    |
++------------+---------------------------------------+
+| open tool  |  Lets you choose what tool too open   |
++------------+---------------------------------------+
+| tools      |  Shows the list of tools              |
++------------+---------------------------------------+
+| info       |  Shows credits for xTools             |
++------------+---------------------------------------+
+| bash       |  Opens a bash session to run commands |
++------------+---------------------------------------+
 ''' + color['DEFAULT']
 header = color['YELLOW'] + '''
-       ___________            __          
-___  __\__    ___/___   ____ |  |   ______
-\  \/  / |    | /  _ \ /  _ \|  |  /  ___/
- >    <  |    |(  <_> |  <_> )  |__\___ \ 
-/__/\_ \ |____| \____/ \____/|____/____  >
-      \/                               \/                            
-''' + color['DEFAULT'] + '''\n                        -=-=-=-=-=-=-=-=-=-=-=-=-\n                          xTools ''' + version + '''\n                          Created by ''' + color['TWITTER_BLUE'] + '''@_Synthx''' + color['DEFAULT'] + '''\n                        -=-=-=-=-=-=-=-=-=-=-=-=-
+                        ___________            __          
+                 ___  __\__    ___/___   ____ |  |   ______
+                 \  \/  / |    | /  _ \ /  _ \|  |  /  ___/
+                  >    <  |    |(  <_> |  <_> )  |__\___ \ 
+                 /__/\_ \ |____| \____/ \____/|____/____  >
+                       \/                               \/                            ''' + color['DEFAULT'] + '''\n                            +-----------------------+\n                            |  Created by ''' + color['TWITTER_BLUE'] + '''@_Synthx''' + color['DEFAULT'] + '''  |\n                            +-----------------------+
 '''
 os.system('clear')
-print(header)
-print('Type "help" to begin!\n')
+print(header + 'Type "help" to begin!\n')
 def Main():
 	global exit
 	mainInput = input(color['PURPLE'] + 'xTools> ' + color['DEFAULT'])
@@ -62,6 +68,7 @@ def Main():
 		Main()
 	elif mainInput.lower() == 'open tool':
 		os.system('clear')
+		print(header + 'Which tool would you like to open?' + toolList)
 		toolOpen()
 	elif mainInput.lower() == 'exit':
 		os.system('clear')
@@ -69,8 +76,7 @@ def Main():
 		exit = True
 	elif mainInput.lower() == 'info':
 		os.system('clear')
-		print(header)
-		print(credits)
+		print(header + '\n' + credits)
 		Main()
 	elif mainInput.lower() == 'clear':
 		os.system('clear')
@@ -78,19 +84,25 @@ def Main():
 		Main()
 	elif mainInput.lower() == 'tools':
 		os.system('clear')
-		print(header)
 		print(listTools)
 		Main()
+	elif mainInput.lower() == 'bash':
+		os.system('clear')
+		print(header + 'Type "' + color['MAGENTA'] + 'exit' + color['DEFAULT'] + '" to end the bash session!')
+		bashSession()
+	elif mainInput.lower() == 'python':
+		os.system('clear')
+		print(header + 'Which version of python?\n' + pythonVersions)
+		pythonSession()
 	else:
 		os.system('clear')
-		print(header)
-		print(color['RED'] + '[ERROR] COMMAND "' + mainInput + '" NOT FOUND\n' + color['DEFAULT'] + 'PLEASE USE A PROPER COMMAND!\n')
+		print(header + color['RED'] + '[ERROR] COMMAND "' + mainInput + '" NOT FOUND\n' + color['DEFAULT'] + 'PLEASE USE A PROPER COMMAND!\n')
 		Main()
 def toolOpen():
-	print(header)
-	print('Which tool would you like to open?' + toolList)
 	mainInput = input(color['PURPLE'] + '> ' + color['DEFAULT'])
-	if mainInput == '1':
+	if mainInput == '':
+		toolOpen()
+	elif mainInput == '1':
 		os.system('clear')
 		os.system('python3 ./tools/folder-locker.py')
 		print(header)
@@ -98,6 +110,7 @@ def toolOpen():
 	elif mainInput == '2':
 		os.system('clear')
 		print(color['RED'] + 'Easy SSH is not ready yet!!!' + color['DEFAULT'])
+		os.system('python3 ./tools/easy_ssh.py')
 		print(header)
 		Main()
 	elif mainInput.lower() == 'cancel':
@@ -106,7 +119,31 @@ def toolOpen():
 		Main()
 	else:
 		os.system('clear')
-		print(color['RED'] + '[ERROR]' + color['DEFAULT'] + 'That is not an option. If you would like to cancel please type "cancel".\nOtherwise, please select a number from the list!')
+		print(header + color['RED'] + '\n[ERROR]' + color['DEFAULT'] + 'That is not an option. If you would like to cancel please type "cancel".\nOtherwise, please select a number from the list!\n' + toolList)
 		toolOpen()
+def bashSession():
+	os.system('bash;clear')
+	print(header)
+	Main()
+def pythonSession():
+	mainInput = input(color['PURPLE'] + '> ' + color['DEFAULT'])
+	if mainInput == '':
+		pythonSession()
+	elif mainInput == '1':
+		os.system('clear;python;clear')
+		print(header)
+		Main()
+	elif mainInput == '2':
+		os.system('clear;python3;clear')
+		print(header)
+		Main()
+	elif mainInput.lower() == 'cancel':
+		os.system('clear')
+		print(header)
+		Main()
+	else:
+		os.system('clear')
+		print(header + color['RED'] + '[ERROR]' + color['DEFAULT'] + 'That is not an option. If you would like to cancel please type "cancel".\nOtherwise, please select a number from the list!\n' + pythonVersions)
+		pythonSession()
 while exit == False:
     Main()
